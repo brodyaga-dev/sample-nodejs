2b6bf281853459dc9a95c5e12af8f4e4b60c4f27
alexander <brodyaga.dev.lab@gmail.com>
2022-01-03 23:37:40 +0300

    f

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
