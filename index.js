4b0477ced8f93d9c5b7be475d4c85503f0ec367b
alexander <brodyaga.dev.lab@gmail.com>
2022-01-03 23:15:28 +0300

    fix

--- index.js
+++ index.js
@@ -17,4 +17,35 @@ var lorem = new LoremIpsum({
 
 app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
 
+var ingredients = [{
+    "id": "234kjw",
+    "text": "Eggs"
+  },
+  {
+    "id": "as82w",
+    "text": "Milk"
+  },
+  {
+    "id": "234sk1",
+    "text": "Bacon"
+  },
+  {
+    "id": "ppo3j3",
+    "text": "Frog Legs"
+  }
+];
+
+
+app.get('/ingredients', function (req, res) {
+  console.log("GET From SERVER");
+  res.send(ingredients);
+});
+
+app.post('/ingredients', function (req, res) {
+  var ingredient = req.body;
+  console.log(req.body);
+  ingredients.push(ingredient);
+  res.status(200).send("Successfully posted ingredient");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
